ea3115b3ba951e13a56e5bcf5a621654054d9bcc
Shane <shane.white487@gmail.com>
2018-10-23 16:26:50 -0400

    Tested for blank lines separating numbers in file

--- ocr.py
+++ ocr.py
@@ -3,5 +3,12 @@
 class OCR():
 
     def __init__(self, file):
+        self.lines = self.read_lines(file)
+
+    def read_lines(self, file):
         with open(file) as accounts:
-            self.lines = accounts.readlines()
+            lines = accounts.readlines()
+        #remove end of line characters
+        for i in range(len(lines)):
+            lines[i] = lines[i].replace('\n', '') 
+        return lines
--- test_ocr.py
+++ test_ocr.py
@@ -8,8 +8,18 @@ class TestOCR(unittest.TestCase):
     
     def test_LinesReadFromFileAreMultipleOfFour(self):
         self.assertNotEqual(len(self.ocr.lines), 0) #make sure lines isn't empty
-        self.assertEqual(len(self.ocr.lines) % 4, 0) #make sure formatting of each entry is the corret length
+        self.assertEqual(len(self.ocr.lines) % 4, 0) #make sure formatting of each entry is the correct length
     
+    def test_EveryFourthLineShouldBeBlank(self):
+        multiple_of_four_line = []
+        for i in range(3, len(self.ocr.lines)):
+            if (i + 1) % 4 == 0:
+                multiple_of_four_line.append(self.ocr.lines[i])
+                print(multiple_of_four_line)
+        for j in range(len(multiple_of_four_line)):
+            self.assertNotEqual(len(multiple_of_four_line[j]), 0) #make sure line wasn't already empty
+            multiple_of_four_line[j] = multiple_of_four_line[j].replace(' ', '') 
+            self.assertEqual(len(multiple_of_four_line[j]), 0)
 
 if __name__ == "__main__":
     unittest.main()
